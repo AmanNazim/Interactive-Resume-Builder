8213675b34b23edc25830b1e12041dcf4c85e4b6
AmanNazim <aman.nazim07@gmail.com>
2024-12-29 03:27:55 +0500

    Rename HTML File

--- Scripts/main.ts
+++ Scripts/main.ts
@@ -1,74 +1,90 @@
 class SkillsListManager {
-    private skillsInputList: HTMLOListElement;
-    // private getSkillInputs(): NodeListOf<HTMLInputElement>;
-    private inputAddButton: HTMLButtonElement;
-    private maxAdds: number;
+  private skillsInputList: HTMLOListElement;
+  // private getSkillInputs(): NodeListOf<HTMLInputElement>;
+  private inputAddButton: HTMLButtonElement;
+  private maxAdds: number;
 
-    constructor (inputList: HTMLOListElement, /*getSkillInputs(): NodeListOf<HTMLInputElement>,*/ addButton: HTMLButtonElement, maxAdds:number) {
-        this.skillsInputList = inputList;
-        // this.getSkillInputs() = getSkillInputs();
-        this.inputAddButton = addButton;
-        this.maxAdds = maxAdds;
+  constructor(
+    inputList: HTMLOListElement,
+    /*getSkillInputs(): NodeListOf<HTMLInputElement>,*/ addButton: HTMLButtonElement,
+    maxAdds: number
+  ) {
+    this.skillsInputList = inputList;
+    // this.getSkillInputs() = getSkillInputs();
+    this.inputAddButton = addButton;
+    this.maxAdds = maxAdds;
 
-        this.eventInitialize();
+    this.eventInitialize();
+  }
+
+  private eventInitialize(): void {
+    this.inputAddButton.addEventListener("click", () => {
+      this.addSkillsInputs();
+    });
+    this.skillsInputList.addEventListener("keydown", (event) => {
+      this.backspaceInputsRemove(event);
+    });
+  }
+
+  private addSkillsInputs(): void {
+    //defining a new inputs variable to store and work getInputsSection gives it an id of HTMLInputElement.
+    const inputs = this.getSkillInputs();
+
+    if (inputs.length >= this.maxAdds) return;
+
+    const newInput = document.createElement("li");
+    newInput.innerHTML = '<input type="text" class="skill-inputs">';
+
+    this.skillsInputList.insertBefore(newInput, this.inputAddButton);
+
+    const newInputField = newInput.querySelector("input") as HTMLInputElement;
+    newInputField.focus();
+
+    if (inputs.length + 1 === this.maxAdds) {
+      this.addButtonVisibility(false);
     }
+  }
 
-    private eventInitialize(): void {
-        this.inputAddButton.addEventListener('click', () => {this.addSkillsInputs()});
-        this.skillsInputList.addEventListener('keydown', (event) => {this.backspaceInputsRemove(event);});
-    }
+  private backspaceInputsRemove(event: KeyboardEvent): void {
+    const targetElement = event.target as HTMLInputElement;
 
-    private addSkillsInputs(): void {
-        //defining a new inputs variable to store and work getInputsSection gives it an id of HTMLInputElement. 
-        const inputs = this.getSkillInputs();
+    if (event.key === "Backspace" && !targetElement.value) {
+      const inputs = this.getSkillInputs();
 
-        if (inputs.length >= this.maxAdds) return;
+      if (inputs.length > 1) {
+        const parentElement = targetElement.parentElement as HTMLLIElement;
+        parentElement?.remove();
 
-        const newInput = document.createElement('li');
-        newInput.innerHTML = '<input type="text" class="skill-inputs">';
-
-        this.skillsInputList.insertBefore(newInput, this.inputAddButton);
-
-        const newInputField = newInput.querySelector('input') as HTMLInputElement;
-        newInputField.focus();
-
-        if (inputs.length + 1 === this.maxAdds) {
-                this.addButtonVisibility(false);
+        if (inputs.length <= this.maxAdds) {
+          this.addButtonVisibility(true);
         }
+      }
     }
+  }
 
-    private backspaceInputsRemove(event: KeyboardEvent): void {
-        const targetElement = event.target as HTMLInputElement;
+  // Get all current skill inputs
+  private getSkillInputs(): NodeListOf<HTMLInputElement> {
+    return this.skillsInputList.querySelectorAll(".skill-inputs");
+  }
 
-        if (event.key === 'Backspace' && !targetElement.value) {
-            const inputs = this.getSkillInputs();
-            
-            if (inputs.length > 1) {
-                const parentElement = targetElement.parentElement as HTMLLIElement;
-                parentElement?.remove();
-
-                if (inputs.length <= this.maxAdds) {
-                    this.addButtonVisibility(true);
-                }
-            }
-        }
-    }
-
-    // Get all current skill inputs
-    private getSkillInputs(): NodeListOf<HTMLInputElement> {
-      return this.skillsInputList.querySelectorAll('.skill-inputs');
-    }
-
-    private addButtonVisibility(visible: boolean): void {
-        this.inputAddButton.style.display = visible ? 'flex' : 'none';
-    }
+  private addButtonVisibility(visible: boolean): void {
+    this.inputAddButton.style.display = visible ? "flex" : "none";
+  }
 }
 
-const skillsInputList = document.querySelector('#skills-input-list') as HTMLOListElement ;
+const skillsInputList = document.querySelector(
+  "#skills-input-list"
+) as HTMLOListElement;
 // const skillsInputs = document.querySelector('.skill-inputs') as NodeListOf<HTMLInputElement>;
-const inputsAddButton = document.querySelector('#add-skill-inputs-button') as HTMLButtonElement;
-const maxInputsAddition:number = 5; 
+const inputsAddButton = document.querySelector(
+  "#add-skill-inputs-button"
+) as HTMLButtonElement;
+const maxInputsAddition: number = 5;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const skillsListManager = new SkillsListManager(skillsInputList, /*skillsInputs,*/ inputsAddButton, maxInputsAddition);
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const skillsListManager = new SkillsListManager(
+    skillsInputList,
+    /*skillsInputs,*/ inputsAddButton,
+    maxInputsAddition
+  );
+});
